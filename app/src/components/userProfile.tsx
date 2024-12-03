403ccfb6dc5f109c51e9ee181977e95e40958c2d
Jeffery Xie <jeffxie27@outlook.com>
2024-12-03 14:41:01 -0600

    Refactor user profile and create brand new components for better maintainability.

--- app/src/components/userProfile.tsx
+++ app/src/components/userProfile.tsx
@@ -2,7 +2,11 @@ import "../styles/userProfile.css";
 
 import { useState, useEffect } from 'react';
 import browser from 'webextension-polyfill';
+
 import Navbar from "./navbar";
+import AboutSection from "./aboutSection";
+import SkillsSection from "./skillsSection";
+import ExperienceSection from "./experienceSection";
 
 interface UserProfileInterface {
   hideUserProfile: Function
@@ -11,55 +15,8 @@ interface UserProfileInterface {
 const UserProfile = (props: React.PropsWithChildren<UserProfileInterface>) => {
   const { hideUserProfile } = props;
   
-  const [activeTab, setActiveTab] = useState("About");
-  const [skills, setSkills] = useState<string[]>([]);
-  const [skillInput, setSkillInput] = useState('');
-  const [coverLetterText, setCoverLetterText] = useState('');
-  
-  useEffect(() => {
-    const keywords = [
-      "Python", 
-      "JavaScript", 
-      "AWS", 
-      "Machine Learning", 
-      "Node.js",
-      "Angular",
-      "SQL"
-    ];
-
-    // Load existing skills
-    const loadSkills = () => {
-      const result = localStorage.getItem("userInfo")
-      
-      let skills = JSON.parse(result || '[]');
-      if (skills.length === 0) {
-        skills = keywords;
-        localStorage.setItem("userInfo", JSON.stringify(skills));
-      }
-    
-      setSkills(skills);
-    };
-
-    loadSkills();
-  }, []);
-
-  const clearSkills = () => {
-    localStorage.removeItem('userInfo');
-    setSkills([]);
-  }
-
-  const addSkill = () => {
-    if (skillInput.trim()) {
-      const currentSkills = [...skills];
-      if (!currentSkills.includes(skillInput)) {
-        currentSkills.push(skillInput);
-        localStorage.setItem("userInfo", JSON.stringify(currentSkills));
-        setSkills(currentSkills);
-      }
-
-      setSkillInput('');
-    }
-  }
+  const [activeTab, setActiveTab] = useState("Work Experience");
+  // const [coverLetterText, setCoverLetterText] = useState('');
     
   return (
     <div className="overlay">
@@ -73,143 +30,23 @@ const UserProfile = (props: React.PropsWithChildren<UserProfileInterface>) => {
             </h3>
 
             <p className="section-text">
-              {
-                activeTab === 'About' ? "This information will be used to personalize cover letters." :
-                activeTab === 'Skills' ? "Skills will be matched with job descriptions to assess fit and personalize cover letters." :
-                "Work experience and achievements that are relevant to the job description will be included in cover letters."
-               }
+              {activeTab === 'About' && "This information will be used to personalize cover letters."} 
+              {activeTab === 'Skills' && "Skills will be matched with job descriptions to assess fit and personalize cover letters."} 
+              {activeTab === 'Work Experience' && "Work experience and achievements that are relevant to the job description will be included in cover letters."}
             </p>
           </div>
 
           <div className="wrapper">
-            {activeTab === "About" && 
-              <div className="about-section">
-                <h3>ABOUT</h3>
-
-                <div className="input-container">
-                  <h4 className="input-header">Name - </h4>
-                  <input placeholder="First and Last Name..."></input>
-                </div>
-                
-                <div className="input-container">
-                  <h4 className="input-header">Location - </h4>
-                  <input placeholder="City, Province / State..."></input>
-                </div>
-
-                <div className="input-container">
-                  <h4 className="input-header">Email Address - </h4>
-                  <input placeholder="Your Email Address..."></input>
-                </div>
-
-                <div className="education-container">
-                  <h3>EDUCATION</h3>
-
-                  <div className="input-container">
-                    <h4 className="input-header">Degree Title - </h4>
-                    <input placeholder="Diploma / Degree..."></input>
-                  </div>
-
-                  <div className="input-container">
-                    <h4 className="input-header">School / University - </h4>
-                    <input placeholder="School / University..."></input>
-                  </div>
-                </div>
-
-                <div>
-                  <h3>CERTIFICATES</h3>
-                </div>
-              </div>
-            }
-        </div>
-
-          {/* 
-
-          <div className="skills-section">
-            <h2
-              className="skills-title"
-              title="Skills will be matched with job descriptions to assess fit and personalize cover letters."
-            >
-              KEY SKILLS
-            </h2>
-
-            <div className="edit-skill-wrapper">
-              <div className="add-skill-wrapper">
-                <input 
-                  className="add-skill-input"
-                  placeholder='Add a skill... (ex. Microsoft Word)'
-                  value={skillInput}
-                  onChange={(e) => setSkillInput(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') addSkill();
-                  }}
-                />
-
-                <button 
-                  className="add-skill-button"
-                  onClick={addSkill}
-                >
-                  Add Skill
-                </button>
-              </div>
-
-              <button 
-                className="clear-skills-button"
-                onClick={clearSkills}
-              >
-                Clear Skills
-              </button>
-            </div>
-
-
-            <div className="skills-wrapper">
-              {skills.map((skill, index) => (
-                <button
-                  key={index}
-                  className="skill"
-                >
-                  {skill}
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div className="experience-section">
-          <h2 
-            className="experience-title" 
-            title="Work experience and achievements that are relevant to the job description will be included in cover letters."
-          >
-            WORK EXPERIENCE AND ACHIEVEMENTS
-          </h2>
-
-          <div className="job-container">
-            <input placeholder="Company Name..."></input>
-            <input placeholder="Job Title..."></input>
+            {activeTab === "About" && <AboutSection/>}
+            {activeTab === "Skills" && <SkillsSection/>}
+            {activeTab === 'Work Experience' && <ExperienceSection/>}
           </div>
 
-          <input placeholder="List work responsibilities and achievements..."></input>
-        </div>
-
-        <p 
-          className="profile-explanation-button"
-          title="Completing your profile helps the extension create more tailored cover letters and better match your skills with job descriptions. You can skip any input field that you want, but it may impact the performance of the extension."
-        >
-          Why fill this out?
-        </p>
-
-        <div className="button-container">
-          <button 
+          {/* <button 
             className="exit-profile-button"
             onClick={() => hideUserProfile()}
           >
             Exit Profile
-          </button>
-
-          <button
-            className="save-changes-button"
-            onClick={() => {}}
-          >
-            Save Changes
           </button> */}
         </div>
       </div>
