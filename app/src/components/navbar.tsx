fadfba95a356b0b7aac9e5f489ef8bf7d457635b
Jeffery Xie <jeffxie27@outlook.com>
2025-01-10 15:47:44 -0600

    Only show navbar items if user is authenticated.

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -8,11 +8,12 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 interface NavbarProps {
   activeTab: string,
   setActiveTab: Function,
+  isAuthenticated: boolean,
   hideUserProfile: Function
 }
 
 const Navbar = (props: React.PropsWithChildren<NavbarProps>) => {
-  const { activeTab, setActiveTab, hideUserProfile } = props;
+  const { activeTab, setActiveTab, isAuthenticated, hideUserProfile } = props;
 
   return (
     <div className="navbar-container">
@@ -21,33 +22,35 @@ const Navbar = (props: React.PropsWithChildren<NavbarProps>) => {
         <h3 className="logo-name">PALLAS</h3>
       </div>
 
-      <div className="navbar-item-container">
-        <div className={`${activeTab === "About" ? 'active-tab' : 'navbar-item'}`} onClick={() => setActiveTab("About")}>
-          <AccountCircleIcon></AccountCircleIcon>
-          <p className="item-text">About Me</p>
+      {isAuthenticated &&
+        <div className="navbar-item-container">
+          <div className={`${activeTab === "About" ? 'active-tab' : 'navbar-item'}`} onClick={() => setActiveTab("About")}>
+            <AccountCircleIcon></AccountCircleIcon>
+            <p className="item-text">About Me</p>
+          </div>
+
+          <div className={`${activeTab === "Resume" ? 'active-tab' : 'navbar-item'}`} onClick={() => setActiveTab("Resume")}>
+            <WorkIcon></WorkIcon>
+            <p className="item-text">Resume</p>
+          </div>
+
+          <p 
+            className="profile-explanation-text"
+            title="Completing your profile helps the extension create more detailed cover letters and better match your skills with job descriptions. You may skip any input field as you please, but it may impact the extension's performance."
+          >
+            Why fill this out?
+          </p>
+
+          <Button 
+            className="exit-button" 
+            color="error"
+            variant="outlined"
+            onClick={() => hideUserProfile()}
+          >
+            EXIT
+          </Button>
         </div>
-
-        <div className={`${activeTab === "Resume" ? 'active-tab' : 'navbar-item'}`} onClick={() => setActiveTab("Resume")}>
-          <WorkIcon></WorkIcon>
-          <p className="item-text">Resume</p>
-        </div>
-      </div>
-
-      <p 
-        className="profile-explanation-text"
-        title="Completing your profile helps the extension create more detailed cover letters and better match your skills with job descriptions. You may skip any input field as you please, but it may impact the extension's performance."
-      >
-        Why fill this out?
-      </p>
-
-      <Button 
-        className="exit-button" 
-        color="error"
-        variant="outlined"
-        onClick={() => hideUserProfile()}
-      >
-        EXIT
-      </Button>
+      }
     </div>
   )
 }
